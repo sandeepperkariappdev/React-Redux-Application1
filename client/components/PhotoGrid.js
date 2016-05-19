fec1ab9700aa2a9429149e2a85eb5fdd45d56cfa
Sandeep Perkari <sandeep.perkari@gmail.com>
2016-05-19 13:27:28 -0500

    Initial commit
    working React-Redux application

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Photo from './Photo';
+import { Link } from 'react-router';
+
+const PhotoGrid = React.createClass({
+   render(){
+       return (
+           <div className="photo-grid">
+                {this.props.posts.map((post, i) => <Photo {... this.props} key={i} i={i} post={post}/>)}
+           </div>
+       )
+   } 
+});
+
+export default PhotoGrid;
